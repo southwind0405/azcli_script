282fb30dbbdb623e619cb83b3d82fdfc6e884646
southwind <southwind.chen@gmail.com>
2025-06-12 00:54:55 +0800

    image.sh

---
+++ image.sh
@@ -0,0 +1,12 @@
+# Linux
+UBUNTU_22="Canonical:0001-com-ubuntu-server-jammy:22_04-lts-gen2:latest"
+DEBIAN_12="Debian:debian-12:12-gen2:latest"
+ORACLE_95="Oracle:Oracle-Linux:ol95-lvm-gen2:9.5.2"
+
+# Windows
+WIN_2019="MicrosoftWindowsServer:WindowsServer:2019-datacenter-gensecond:latest"
+WIN_2022="MicrosoftWindowsServer:WindowsServer:2022-datacenter-azure-edition:latest"
+
+# RedHat / CentOS
+RHEL_8="RedHat:RHEL:8_6:latest"
+CENTOS_7="OpenLogic:CentOS:7_9-gen2:latest"
